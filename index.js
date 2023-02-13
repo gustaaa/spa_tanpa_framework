142d2212fff9015c4269795c38f1ff0150ce02e1
gustaaa <gustayo24@gmail.com>
2023-02-13 13:35:29 +0700

    app component

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const root = document.getElementById("root");
-
 function link(props) {
     const link = document.createElement("a");
     link.href = props.href;
     link.textContent = props.label;
     link.onclick = function (event) {
         event.preventDefault();
-        const component = props.Component();
-        root.innerHTML = "";
-        root.append(component);
         history.pushState(null, "", event.target.href);
+        render();
     };
 
     return link;
@@ -19,13 +15,11 @@ function Navbar() {
     const linkHome = link({
         href: "#home",
         label: "Home",
-        Component: HomeScreen,
     });
 
     const linkAbout = link({
         href: "#about",
         label: "About",
-        Component: AboutScreen,
     });
 
     const div = document.createElement("div");
@@ -39,7 +33,6 @@ function AboutScreen() {
     const linkHome = link({
         href: "#home",
         label: "Kembali ke Home",
-        Component: HomeScreen,
     });
 
     const text = document.createElement("p");
@@ -71,12 +64,22 @@ function HomeScreen() {
     return div;
 }
 
-if (location.hash == "#about") {
-    const aboutScreen = AboutScreen();
-    root.innerHTML = "";
-    root.append(aboutScreen);
-} else if (location.hash == "#home") {
+function App() {
     const homeScreen = HomeScreen();
-    root.innerHTML = "";
-    root.append(homeScreen);
+    const aboutScreen = AboutScreen();
+
+    if (location.hash == "#about") {
+        return aboutScreen;
+    } else if (location.hash == "#home") {
+        return homeScreen;
+    }
 }
+
+function render() {
+    const root = document.getElementById("root");
+    const app = App();
+    root.innerHTML = "";
+    root.append(app);
+}
+
+render();
